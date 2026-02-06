56b5fb3170fd72a60924129312f6ec94da801ed8
Daniela <danielarodrigues2po@gmail.com>
2026-02-06 17:48:52 -0300

    html + css base da página

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,13 +3,23 @@ import './App.css'
 
 function App() {
 
+  const [busca, setBusca] = useState('')
+
   return (
     <div className='principal'>
       <header className='header'>
           <h1>Gerenciamento de Clientes</h1>
       </header>
+      
       <main className='main'>
-        <div className='busca'></div>
+        <div className='busca'>
+          <input value={busca} onChange={(e) => setBusca(e.target.value)} className='barraDeBusca'></input>
+          <select className='dropDown'>
+            <option value='nome'>Nome</option>
+            <option value='email'>Email</option>
+            <option value='cidade'>Cidade</option>
+          </select>
+        </div>
         <div className='lista'></div>
       </main>
 
