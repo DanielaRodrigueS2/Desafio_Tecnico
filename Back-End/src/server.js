ef5abd5b499244323547030cdd1078a0de291ac2
Daniela <danielarodrigues2po@gmail.com>
2026-02-07 17:19:37 -0300

    fix conexao mongo

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -2,10 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: ture
-})
+mongoose.connect(process.env.MONGO_URI, {})
 .then(()=>{
     console.log('Conexao com Mongo foi feita com sucesso :p');
     app.listen(3000, () =>{
